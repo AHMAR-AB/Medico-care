79c78374b0be9cba281b6a4518cdfcb3d180375b
Ahmar <ahmarabdullah636@gmail.com>
2021-02-07 19:31:09 +0530

    my initial commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const responsive={
+    0:
+    {
+        items:1
+    },
+    320:{
+        items:1
+    },
+    560:
+    {
+        items:2
+    },
+    960:
+    {
+        items:3
+    }
+}
+$(document).ready(function()
+{
+$nav = $('.nav');
+$toggleCollapse = $('.toggle-collapse');
+/*click event on toggle menu*/
+$toggleCollapse.click(function()
+{
+$nav.toggleClass('collapse');
+})
+
+
+
+//owl carosel//
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    responsive:responsive
+    
+});
+//aos instance
+AOS.init();
+});
+
+
+
+
